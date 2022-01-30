fc7c6e688163806a4536a35514443ba0fbbd6b0e
Vukasin Grebic <vgrebic@gmail.com>
2022-01-30 14:58:48 +0100

    Init commit

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.navigate().to("https://www.google.com/");
+	}
+
+}
