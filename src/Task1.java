fa5e043b0cdc1947c022824aea77a01d6aefbff3
Vukasin Grebic <vgrebic@gmail.com>
2022-01-30 15:47:13 +0100

    Added alternative

--- src/Task1.java
+++ src/Task1.java
@@ -1,7 +1,9 @@
+import java.util.List;
 import java.util.Scanner;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task1 {
@@ -14,14 +16,27 @@ public class Task1 {
 		int n = 0;
 		driver.navigate().to("https://s.bootsnipp.com/iframe/WaXlr");
 		driver.manage().window().maximize();
+		
+		List <WebElement> stars = driver.findElements(By.xpath("//*[@class='form-group']/button"));
+		
 		System.out.println("Enter number (1-5): ");
 		n = s.nextInt();
+		
+		
+//		if (n <= 5) {
+//			driver.findElement(By.id("rating-star-" + Integer.toString(n))).click();
+//			Thread.sleep(5000);
+//		} else {
+//			System.out.println("Wrong number");
+//		}
+		
 		if (n <= 5) {
-			driver.findElement(By.id("rating-star-" + Integer.toString(n))).click();
+			stars.get(n-1).click();
 			Thread.sleep(5000);
 		} else {
 			System.out.println("Wrong number");
 		}
+		
 		driver.close();
 		
 
