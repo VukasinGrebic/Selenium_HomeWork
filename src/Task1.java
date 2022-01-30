625d242775b367bed2d4507dffcb4ff30875c864
Vukasin Grebic <vgrebic@gmail.com>
2022-01-30 15:16:51 +0100

    Added Task1

---
+++ src/Task1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		Scanner s = new Scanner(System.in);
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		int n = 0;
+		driver.navigate().to("https://s.bootsnipp.com/iframe/WaXlr");
+		driver.manage().window().maximize();
+		System.out.println("Enter number (1-5): ");
+		n = s.nextInt();
+		if (n <= 5) {
+			driver.findElement(By.id("rating-star-" + Integer.toString(n))).click();
+			Thread.sleep(5000);
+		} else {
+			System.out.println("Wrong number");
+		}
+		driver.close();
+		
+
+	}
+
+}
+
+//Maksimizirati prozor
+//Ucitati stranicu https://s.bootsnipp.com/iframe/WaXlr
+//Od korisnika ucitati broj na koju zvezdicu je potrebno kliknuti (u rasponu od 1 do 5)
+//I izvrsite akciju klik na odgovarajuci element
+//Na kraju programa ugasite pretrazivac.
