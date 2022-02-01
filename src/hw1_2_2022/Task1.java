356b499b07440274ff2f86283904511634ad3515
Vukasin Grebic <vgrebic@gmail.com>
2022-02-01 23:45:11 +0100

    Added task 1 and task 2

---
+++ src/hw1_2_2022/Task1.java
@@ -0,0 +1,43 @@
+package hw1_2_2022;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://s.bootsnipp.com/iframe/Dq2X");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		
+		List <WebElement> windows= driver.findElements(By.className("close"));
+
+		for (int i = 0; i < windows.size(); i++) {
+			windows.get(i).click();
+			try {
+				windows.get(i).click();
+
+			} catch (Exception e) {
+				System.out.println("Closed");
+			}
+		}
+
+	}
+
+}
+
+//Napisati program koji:
+//Ucitava stranicu https://s.bootsnipp.com/iframe/Dq2X
+//Klikce na svaki iks da ugasi obavestenje i proverava da li se nakon klika element obrisao sa stranice i ispisuje odgovarajuce poruke (OVO JE POTREBNO RESITI KORISCENJEM PETLJE)
+//POMOC: Brisite elemente odozdo.
+//(ZA VEZBANJE)Probajte da resite da se elemementi brisu i odozgo
---
+++ src/hw1_2_2022/Task2.java
@@ -0,0 +1,41 @@
+package hw1_2_2022;
+
+import java.io.File;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://geodata.solutions/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		Select select = new Select(driver.findElement(By.id("countryId")));
+		select.selectByIndex(1);
+
+		Select select1 = new Select(driver.findElement(By.id("stateId")));
+		select1.selectByIndex(1);
+
+
+		Select select2 = new Select(driver.findElement(By.id("cityId")));
+
+		select2.selectByIndex(1);
+
+	}
+
+}
+
+//adatak
+//Napisati program koji ucitava stranicu https://geodata.solutions/
+//Bira proizvoljan Country, State i City
+//Pritom potrebno je izvrsiti cekanje da se povaje State-ovi nakon izbora Country-a
+//I takodje je potrebno izvrsiti cekanje da se ucitaju gradovi nakon izbora State-a
+//Izabrerit Country, State i City tako da imate podatke da selektujete!
