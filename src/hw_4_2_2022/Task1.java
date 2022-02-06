ca2b94b323c49bcc33c5842926a7ff655a701022
Vukasin Grebic <vgrebic@gmail.com>
2022-02-06 18:42:30 +0100

    Updated task1

--- src/hw_4_2_2022/Task1.java
+++ src/hw_4_2_2022/Task1.java
@@ -1,7 +1,7 @@
 package hw_4_2_2022;
 
 import java.time.Duration;
-
+import java.util.List;
 
 import org.openqa.selenium.By;
 
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
 import org.testng.annotations.BeforeMethod;
@@ -20,6 +20,7 @@ import org.testng.annotations.Test;
 public class Task1 {
 	private WebDriver driver;
 	private WebDriverWait wait;
+
 
 	
 	@BeforeMethod
@@ -47,12 +48,26 @@ public class Task1 {
 		}
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("welcome-holder")));
 		driver.findElement(By.className("searchKeywordsInput")).sendKeys("iphone");
+		wait.until(ExpectedConditions.textToBePresentInElementValue(By.className("searchKeywordsInput"), "iphone"));
 		driver.findElement(By.className("searchButton")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("priceSec")));
 		driver.findElement(By.className("choiceLabel")).click();
 		driver.findElement(By.xpath("//*[@id='menuGroup0']/div")).click();
-//		
+		driver.findElement(By.className("choiceLabelText")).click();
+		driver.findElement(By.xpath("//*[@value='eur']")).click();
+		
+		
+		List<WebElement> prices = driver.findElements(By.className("adPrice "));
+		
+		
+		
+		Assert.assertTrue(firstItemcheaper(prices), "It's in right order");
 
 		
+	}
+	
+	public boolean firstItemcheaper (List<WebElement> prices) {
+		return Double.valueOf(prices.get(0).getText()) < Double.valueOf(prices.get(29).getText());
 	}
 	
 	@AfterMethod
@@ -68,3 +83,8 @@ public class Task1 {
 //postavice za valutu eure
 //Sortirajte prema ceni da bude najjefinije prvo
 //Onda izvrsite proveru sortiranja kao sto je bilo za udemy
+//Dohvatite rejting prvog rezultata pretrage
+//Dohvatite rejting zadnjeg rezultata pretrage (HELP: trebace vam getText metoda)
+//	[HELP] Dohvatite rejting kao listu  pa iz liste izvucite prvi i zadnji element.
+//Proverite da li je reting prvog veci od rejtinga zadnjeg rezultata
+//pretvaranje stringa u double link ce vam trebati za ovaj zadatak
